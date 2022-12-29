087aa8e5512c9486d88f8eaef11860b386a52c1f
Caden Barton <cadenbarton@cadens-air.lan>
2022-12-28 20:31:57 -0700

    Added player input system

--- 2DCorruptionUnityProject/Assets/_Scripts/PlayerController.cs
+++ 2DCorruptionUnityProject/Assets/_Scripts/PlayerController.cs
@@ -1,55 +1,101 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Gamepad connectedGamepad;
-    private Keyboard connectedKeyboard;
-    private Mouse connectedMouse;
+    private Rigidbody2D playerRigidBody;
+    private PlayerInputActions playerInputActions;
+    private float jumpForce;
     private float moveSpeed;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        connectedGamepad = Gamepad.current;
-        connectedKeyboard = Keyboard.current;
-        connectedMouse = Mouse.current;
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump_Performed;
+        playerInputActions.Player.Jump.canceled += Jump_canceled;
+
+        jumpForce = 5.0f;
         moveSpeed = 5.0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (isGamepadConnected()){}
+        HorizontalMovement();
+    }
 
-        if (isKeyboardConnected())
+    private void HorizontalMovement()
+    {
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        transform.Translate(inputVector.x * moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    private void Jump_Performed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
         {
-            
+            playerRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+    }
 
-        if (isMouseConnected())
+    private void Jump_canceled(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
         {
-
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, 0.0f);
         }
-
-        float translation = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        transform.Translate(translation, 0.0f, 0.0f);
     }
 
-    private bool isGamepadConnected()
-    {
-        return connectedGamepad != null;
-    }
+    //private Gamepad connectedGamepad;
+    //private Keyboard connectedKeyboard;
+    //private Mouse connectedMouse;
+    //private float moveSpeed;
 
-    private bool isKeyboardConnected()
-    {
-        return connectedKeyboard != null;
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    connectedGamepad = Gamepad.current;
+    //    connectedKeyboard = Keyboard.current;
+    //    connectedMouse = Mouse.current;
+    //    moveSpeed = 5.0f;
+    //}
 
-    private bool isMouseConnected()
-    {
-        return connectedMouse != null;
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (isGamepadConnected()){}
+
+    //    if (isKeyboardConnected())
+    //    {
+
+    //    }
+
+    //    if (isMouseConnected())
+    //    {
+
+    //    }
+
+    //    float translation = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+    //    transform.Translate(translation, 0.0f, 0.0f);
+    //}
+
+    //private bool isGamepadConnected()
+    //{
+    //    return connectedGamepad != null;
+    //}
+
+    //private bool isKeyboardConnected()
+    //{
+    //    return connectedKeyboard != null;
+    //}
+
+    //private bool isMouseConnected()
+    //{
+    //    return connectedMouse != null;
+    //}
 }
