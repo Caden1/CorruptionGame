ebac04c6e8163b9ec18914374bb3dd77be6c7247
Caden Barton <cadenbarton@cadens-air.lan>
2023-04-27 13:28:39 -0600

    Adding enemy AI

--- 2DCorruptionUnityProject/Assets/_Scripts/Enemies/Corruption/EnemyController.cs
+++ 2DCorruptionUnityProject/Assets/_Scripts/Enemies/Corruption/EnemyController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-	public float speed = 1.0f;
+	public enum EnemyState {
+		Roam,
+		ChasePlayer,
+		AttackPlayer
+	}
+
+	public float speed = 2.0f;
 	public float distance = 5.0f;
-	public float detectionRange = 4.0f;
-	public float verticalDetectionLimit = 2.0f;
+	public float detectionRange = 3.0f;
+	public float verticalDetectionLimit = 1.0f;
+	public float attackRange = 1.0f;
 	public Transform player;
+	public EnemyState currentState;
 
 	private Vector2 startPoint;
 	private Vector2 endPoint;
@@ -20,13 +28,31 @@ public class EnemyController : MonoBehaviour
 		startPoint = rb.position;
 		endPoint = new Vector2(startPoint.x + distance, startPoint.y);
 		currentTarget = endPoint;
+		currentState = EnemyState.Roam;
 	}
 
-    void Update() {
-		if (IsPlayerInRange()) {
-			ChasePlayer();
-		} else {
-			MoveBackAndForth();
+	void Update() {
+		switch (currentState) {
+			case EnemyState.Roam:
+				MoveBackAndForth();
+				if (IsPlayerInRange()) {
+					currentState = EnemyState.ChasePlayer;
+				}
+				break;
+			case EnemyState.ChasePlayer:
+				ChasePlayer();
+				if (!IsPlayerInRange()) {
+					currentState = EnemyState.Roam;
+				} else if (IsPlayerInAttackRange()) {
+					currentState = EnemyState.AttackPlayer;
+				}
+				break;
+			case EnemyState.AttackPlayer:
+				AttackPlayer();
+				if (!IsPlayerInAttackRange()) {
+					currentState = EnemyState.ChasePlayer;
+				}
+				break;
 		}
 	}
 
@@ -34,14 +60,19 @@ public class EnemyController : MonoBehaviour
 		float horizontalDistance = Vector2.Distance(new Vector2(rb.position.x, 0), new Vector2(player.position.x, 0));
 		float verticalDistance = Mathf.Abs(rb.position.y - player.position.y);
 
-		return (horizontalDistance <= detectionRange && verticalDistance <= verticalDetectionLimit);
+		return horizontalDistance <= detectionRange && verticalDistance <= verticalDetectionLimit;
+	}
+
+	bool IsPlayerInAttackRange() {
+		float distanceToPlayer = Vector2.Distance(rb.position, player.position);
+		return distanceToPlayer <= attackRange;
 	}
 
 	void MoveBackAndForth() {
 		float step = speed * Time.deltaTime;
-		
+
 		if (Vector2.Distance(rb.position, currentTarget) < step) {
-			currentTarget = (currentTarget == startPoint) ? endPoint : startPoint;
+			currentTarget = currentTarget == startPoint ? endPoint : startPoint;
 		} else {
 			rb.position = Vector2.MoveTowards(rb.position, currentTarget, step);
 		}
@@ -50,5 +81,9 @@ public class EnemyController : MonoBehaviour
 	void ChasePlayer() {
 		float step = speed * Time.deltaTime;
 		rb.position = Vector2.MoveTowards(rb.position, player.position, step);
+	}
+
+	void AttackPlayer() {
+		rb.position = rb.position;
 	}
 }
