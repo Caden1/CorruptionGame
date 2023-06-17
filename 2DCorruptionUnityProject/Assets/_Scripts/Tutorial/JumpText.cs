948466a379c4f997ed2d147ee4054e3ff5cc7e24
Caden Barton <cadenbarton@cadens-air.lan>
2023-06-17 17:21:51 -0600

    Added tutorial text for holding jump

--- 2DCorruptionUnityProject/Assets/_Scripts/Tutorial/JumpText.cs
+++ 2DCorruptionUnityProject/Assets/_Scripts/Tutorial/JumpText.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class JumpText : MonoBehaviour
 {
+	public string animationName;
+	public GameObject[] animObjectTurnOff;
+
 	private Animator animator;
 
 	private void Start() {
-		animator = GetComponentInParent<Animator>();
+		animator = GetComponent<Animator>();
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision) {
 		if (collision.tag == "Player") {
-			animator.Play("JumpText");
+			if (!string.IsNullOrWhiteSpace(animationName)) {
+				if (animObjectTurnOff != null) {
+					foreach (GameObject el in animObjectTurnOff) {
+						if (el != null) {
+							el.GetComponent<Animator>().Play("EmptyState");
+						}
+					}
+				}
+				animator.Play(animationName);
+			}
 		}
 	}
 }
