1e501708c3f892a5160947d7d792a40878b4ddfe
Caden Barton <cadenbarton@cadens-air.lan>
2023-08-16 16:59:50 -0600

    Added cor realm song

--- 2DCorruptionUnityProject/Assets/_Scripts/AudioManager.cs
+++ 2DCorruptionUnityProject/Assets/_Scripts/AudioManager.cs
@@ -27,7 +27,7 @@ public class AudioManager : MonoBehaviour
 
 	private void Start() {
 		// For Testing:
-		//PlayMusic(0, true);
+		//PlayMusic(1, true);
 	}
 
 	public void PlayMusic(int trackIndex, bool loop = true) {
